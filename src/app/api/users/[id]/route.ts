aaa5e5f1a6d6b1931da76b9fa7695d9d09bb4231
Vivek Sharma <vivek.indutech@gmail.com>
2025-02-24 08:16:02 +0530

    update in user id

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,18 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
+const prisma = new PrismaClient();
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
     try {
+        const { id } = context.params;
         const { name, email } = await req.json();
-        const prisma = new PrismaClient();
+
         const user = await prisma.user.update({
-            where: { id: params.id },
+            where: { id },
             data: { name, email },
         });
+
         return NextResponse.json(user, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error: "Error updating user" }, { status: 500 });
     }
 }
-  
