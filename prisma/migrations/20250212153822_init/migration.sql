9a50dd9feedbed61cd8fb61c0017050e3196ab4c
Vivek Sharma <vivek.indutech@gmail.com>
2025-03-02 09:28:44 +0530

    remove api, prisma

--- prisma/migrations/20250212153822_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Document_userId_fkey` ON `document`;
-
--- AddForeignKey
-ALTER TABLE `Document` ADD CONSTRAINT `Document_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
